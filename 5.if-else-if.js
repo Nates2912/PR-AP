a6ebf6118b6b47a6263d03ec247503552eb5cafa
Nates2912 <srakawaiiuwu@gmail.com>
2026-03-02 16:55:04 -0300

    feat: javascript 3

--- 5.if-else-if.js
+++ 5.if-else-if.js
@@ -1,12 +1,14 @@
 //estruturas condicionais
 
-const idade = 15
+const idade = 19
 
 if(idade >= 18) {
     console.log("Adulto")
 } else if (idade >= 12) {
     console.log("Adolescente")
+} else if (idade >= 2) {
+    console.log("Criança")
 }
 else{
-    console.log("Criança")
+    console.log("Bebê")
 }
