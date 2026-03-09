17bbb0a4eeb2c8b1c32eff75587f67fe800cde02
Nates2912 <srakawaiiuwu@gmail.com>
2026-03-09 16:37:16 -0300

    feat: funcionalidades

---
+++ COMEÇO/15.funcionalidades-ES6.js
@@ -0,0 +1,18 @@
+//novos recursos ES6
+
+//vetor de números
+const listaDeNumeros = [1, 2, 3, 4, 5]
+console.log("Exibindo números do vetor: ")
+console.log(listaDeNumeros)
+
+console.log("\nMultiplicando números do vetor: ")
+const dobrados = listaDeNumeros.map(n => n * 2)
+console.log(dobrados)
+
+console.log("\nFiltrar os números pares do vetor: ")
+const pares = listaDeNumeros.filter(n => n % 2 == 0)
+console.log(pares)
+
+console.log("\nSomando todos os pares do vetor: ")
+const soma = listaDeNumeros.reduce((soma, atual)=> soma + atual, 0)
+console.log(soma)
