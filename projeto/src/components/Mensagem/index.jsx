29ea5c411252a395b8b87a256bbb9da537752b5f
Nates2912 <srakawaiiuwu@gmail.com>
2026-03-26 16:54:13 -0300

    projeto

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+function Mensagem() {
+    return (
+        //comandos HTML
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Estes é um componente com uma mensagem simple.</p>
+        </div>
+    )
+    
+}
